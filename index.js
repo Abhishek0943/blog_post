015b3cc81b3afdb0310e28d5bc3cb86f6afd0a28
abhishek pundir <100474102+Abhishek0943@users.noreply.github.com>
2022-11-26 22:02:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
+
 const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const dotenv = require("dotenv")
-const cors = require('cors')
 const data = require('./routes/root')
-
-
-const app = express()
-dotenv.config({ path: "./config/config.env" })
-app.use('/post', data)
-app.use(bodyParser.json({limit:"30mb", extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
-app.use(cors())
-
-const port = process.env.PORT
-mongoose.connect(process.env.DB_URL).then(() => app.listen(port, () => {
-    console.log(`server runing on ${port}`)
-}))
+const index = express()
+index.use(express.json());
+index.use('/post', data)
+module.exports = index;
